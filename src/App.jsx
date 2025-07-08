ec42ea818ad79a353e41d32c53dfae1eb0dd5515
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-08 19:34:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css"; // Tailwind CSS ko import karna zaroori hai
+
+function App() {
+  return <div className="text-2xl text-black underline">ram ram</div>;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+      },
+    },
+  },
+  plugins: [tailwindcss()],
+});
